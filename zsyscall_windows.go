f9462c22e52327379d5a3e2fde3685ff21101070
Hiroaki Nakakmura <hnakamur@gmail.com>
2015-01-31 03:50:49 +0900

    Add functions for modifying system timezone

--- zsyscall_windows.go
+++ zsyscall_windows.go
@@ -6,14 +6,89 @@ import (
 )
 
 var (
+	modkernel32 = syscall.NewLazyDLL("kernel32.dll")
 	modadvapi32 = syscall.NewLazyDLL("advapi32.dll")
 
-	procRegCreateKeyExW    = modadvapi32.NewProc("RegCreateKeyExW")
-	procRegDeleteKeyValueW = modadvapi32.NewProc("RegDeleteKeyValueW")
-	procRegDeleteTreeW     = modadvapi32.NewProc("RegDeleteTreeW")
-	procRegGetValueW       = modadvapi32.NewProc("RegGetValueW")
-	procRegSetKeyValueW    = modadvapi32.NewProc("RegSetKeyValueW")
+	procGetDynamicTimeZoneInformation = modkernel32.NewProc("GetDynamicTimeZoneInformation")
+	procSetDynamicTimeZoneInformation = modkernel32.NewProc("SetDynamicTimeZoneInformation")
+
+	procAdjustTokenPrivileges = modadvapi32.NewProc("AdjustTokenPrivileges")
+	procLookupPrivilegeValue  = modadvapi32.NewProc("LookupPrivilegeValueW")
+	procRegCreateKeyExW       = modadvapi32.NewProc("RegCreateKeyExW")
+	procRegDeleteKeyValueW    = modadvapi32.NewProc("RegDeleteKeyValueW")
+	procRegDeleteTreeW        = modadvapi32.NewProc("RegDeleteTreeW")
+	procRegGetValueW          = modadvapi32.NewProc("RegGetValueW")
+	procRegSetKeyValueW       = modadvapi32.NewProc("RegSetKeyValueW")
 )
+
+func GetDynamicTimeZoneInformation(timeZoneInformation *DynamicTimeZoneInformation) (err error) {
+	r0, _, e1 := syscall.Syscall(procGetDynamicTimeZoneInformation.Addr(), 1,
+		uintptr(unsafe.Pointer(timeZoneInformation)),
+		0,
+		0)
+	if r0 == TIME_ZONE_ID_INVALID {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func SetDynamicTimeZoneInformation(timeZoneInformation *DynamicTimeZoneInformation) (err error) {
+	r0, _, e1 := syscall.Syscall(procSetDynamicTimeZoneInformation.Addr(), 1,
+		uintptr(unsafe.Pointer(timeZoneInformation)),
+		0,
+		0)
+	if r0 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func AdjustTokenPrivileges(tokenHandle syscall.Token, disableAllPrivileges bool, newState *TokenPrivileges, bufferLength uint32, previousState *TokenPrivileges, returnLength *uint32) (err error) {
+	var _p0 uint32
+	if disableAllPrivileges {
+		_p0 = 1
+	} else {
+		_p0 = 0
+	}
+	r1, _, e1 := syscall.Syscall6(procAdjustTokenPrivileges.Addr(), 6,
+		uintptr(tokenHandle),
+		uintptr(_p0),
+		uintptr(unsafe.Pointer(newState)),
+		uintptr(bufferLength),
+		uintptr(unsafe.Pointer(previousState)),
+		uintptr(unsafe.Pointer(returnLength)))
+	if r1 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func LookupPrivilegeValue(systemName, name *uint16, luid *Luid) (err error) {
+	r1, _, e1 := syscall.Syscall(procLookupPrivilegeValue.Addr(), 3,
+		uintptr(unsafe.Pointer(systemName)),
+		uintptr(unsafe.Pointer(name)),
+		uintptr(unsafe.Pointer(luid)))
+	if r1 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
 
 func RegCreateKeyEx(key syscall.Handle, subkey *uint16, reserved uint32, class *uint16, options uint32, desiredAccess uint32, securityAttributes *syscall.SecurityAttributes, result *syscall.Handle, disposition *uint32) (regerrno error) {
 	r0, _, _ := syscall.Syscall9(procRegCreateKeyExW.Addr(), 9, uintptr(key), uintptr(unsafe.Pointer(subkey)), uintptr(reserved), uintptr(unsafe.Pointer(class)), uintptr(options), uintptr(desiredAccess), uintptr(unsafe.Pointer(securityAttributes)), uintptr(unsafe.Pointer(result)), uintptr(unsafe.Pointer(disposition)))
--- ztypes_windows.go
+++ ztypes_windows.go
@@ -1,5 +1,9 @@
 package w32syscall
 
+import "syscall"
+
+// Registry
+
 const (
 	RRF_RT_ANY           = 0x0000ffff
 	RRF_RT_DWORD         = 0x00000018
@@ -13,4 +17,65 @@ const (
 	RRF_RT_REG_SZ        = 0x00000002
 	RRF_NOEXPAND         = 0x10000000
 	RRF_ZEROONFAILURE    = 0x20000000
+)
+
+// AccessToken
+
+const (
+	ERROR_SUCCESS          = 0
+	ERROR_INVALID_FUNCTION = 1
+	ERROR_NOT_ALL_ASSIGNED = 1300
+)
+
+type Luid struct {
+	LowPart  uint32
+	HighPart int32
+}
+
+type LuidAndAttributes struct {
+	Luid       Luid
+	Attributes uint32
+}
+
+const ANYSIZE_ARRAY = 1
+
+// https://msdn.microsoft.com/ja-jp/library/windows/desktop/aa379630(v=vs.85).aspx
+type TokenPrivileges struct {
+	PrivilegeCount uint32
+	Privileges     [ANYSIZE_ARRAY]LuidAndAttributes
+}
+
+// TimeZone
+
+type DynamicTimeZoneInformation struct {
+	Bias                        int32
+	StandardName                [32]uint16
+	StandardDate                syscall.Systemtime
+	StandardBias                int32
+	DaylightName                [32]uint16
+	DaylightDate                syscall.Systemtime
+	DaylightBias                int32
+	TimeZoneKeyName             [128]uint16
+	DynamicDaylightTimeDisabled uint8 /* BOOLEAN */
+}
+
+const (
+	TIME_ZONE_ID_INVALID  = 0xFFFFFFFF
+	TIME_ZONE_ID_UNKNOWN  = 0
+	TIME_ZONE_ID_STANDARD = 1
+	TIME_ZONE_ID_DAYLIGHT = 2
+)
+
+const SE_TIME_ZONE_NAME = "SeTimeZonePrivilege"
+
+const (
+	SE_PRIVILEGE_ENABLED_BY_DEFAULT = 0x00000001
+	SE_PRIVILEGE_ENABLED            = 0x00000002
+	SE_PRIVILEGE_REMOVED            = 0x00000004
+	SE_PRIVILEGE_USED_FOR_ACCESS    = 0x80000000
+
+	SE_PRIVILEGE_VALID_ATTRIBUTES = SE_PRIVILEGE_ENABLED_BY_DEFAULT |
+		SE_PRIVILEGE_ENABLED |
+		SE_PRIVILEGE_REMOVED |
+		SE_PRIVILEGE_USED_FOR_ACCESS
 )
